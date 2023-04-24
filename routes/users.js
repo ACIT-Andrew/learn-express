d0c3c34714bffdf9bbc64062c08b43653da64696
ACIT-Andrew <115048179+ACIT-Andrew@users.noreply.github.com>
2023-04-24 15:14:48 -0700

    Serving static files, form parsing/json data, parsing query params.

--- routes/users.js
+++ routes/users.js
@@ -2,24 +2,39 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  // e.g. get request from client. send response: "User List"
+  console.log(req.query.name); // req.query gets query parameter
   res.send("User List");
 });
 
+//// Learn form parsing/JSON data here. ////
 router.get("/new", (req, res) => {
-  res.send("User New Form");
-});
-
-router.post("/", (req, res) => {
-  // e.g. post request for creating user
-  res.send("Create User");
+  //   res.send("User New Form");
+  res.render("users/new", { firstName: "Test" });
 });
 
 /* 
 Make sure you always put your static routes (e.g. "/new")
-above dynamic routes, because the static url parameter will be used by the dynamic route first.
-/new will register as an ":id" in this case.
+above dynamic routes (e.g. "/:id"), because if you don't the static url parameter will be used by the dynamic route first.
+"new" will register as an ":id" in this case.
 */
 
+router.post("/", (req, res) => {
+  // e.g. post request for creating user
+  // Here is a mock validation process : 
+  const isValid = true;
+  if (isValid) {
+    users.push({ firstName: req.body.firstName });
+    res.redirect(`users/${users.length - 1}`);
+  } else {
+    console.log("Error");
+    res.render("users/new", { firstName: req.body.firstName });
+  }
+//   console.log(req.body.firstName);
+//   res.send("Create User");
+});
+
+//// Let's work with dynamic urls/routes. ////
 // router.get("/:id", (req, res) => {
 //   // dynamic route parameter ":id"
 //   res.send(`Get User with ID ${req.params.id}`);
@@ -38,7 +53,7 @@ router
   .route("/:id")
   .get((req, res) => {
     // dynamic route parameter ":id"
-    console.log(req.user);
+    // console.log(req.user);
     res.send(`Get User with ID ${req.params.id}`);
   })
   .put((req, res) => {
@@ -50,9 +65,8 @@ router
     res.send(`Delete User with ID ${req.params.id}`);
   });
 
-
 //// MIDDLEWARE ////
-/* Middleware are functions that run between a client request and its server response.
+/* Middleware are functions that run between a client request and its server response. They are run before any request functions are executed.
 router.param() is one example of middleware.
 */
 
@@ -64,7 +78,5 @@ router.param("id", (req, res, next, id) => {
   req.user = users[id]; // Assign request for /user as a users array item, using id parameter
   next();
 });
-
-
 
 module.exports = router;
